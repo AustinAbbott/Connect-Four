8102539a203fe3c727b6c175035689e5aac44b0c
Austin Abbott <magicaustin51547@gmail.com>
2020-05-12 13:18:41 -0600

    Add style sheet

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 class Board extends React.Component {
   render() {
-    return <h1>Hello there!</h1>;
+    return <div id="board" style={{backgroundColor: "gray"}}><h1>Hello there Board!</h1></div>;
   }
 }
 
