6f292b75fb8fd55101bce32901c56e43db06ecca
Austin Abbott <magicaustin51547@gmail.com>
2020-05-12 13:07:47 -0600

    Add Board component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Board from "./app.jsx"
 
-ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById('root'));
+
+ReactDOM.render(<Board />, document.getElementById("root"));
